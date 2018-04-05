f414316abf7ea5f3d0fc2290345df131c2e6b1aa
djordje <przulj@gmail.com>
2018-04-05 22:08:24 +0200

    Dodata faseta JPA, generisana JPA klasa, kreiran repository.

---
+++ L5Data/src/main/java/com/example/l5data/repository/TStoreRepository.java
@@ -0,0 +1,9 @@
+package com.example.l5data.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.l5data.model.TStore;
+
+public interface TStoreRepository extends CrudRepository<TStore, Integer> {
+
+}
