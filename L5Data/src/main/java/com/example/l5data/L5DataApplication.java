fe439b967408dcdb3a10190a74d63e218054be0b
djordje <przulj@gmail.com>
2018-04-05 21:58:17 +0200

    Inicijalni komit.

---
+++ L5Data/src/main/java/com/example/l5data/L5DataApplication.java
@@ -0,0 +1,12 @@
+package com.example.l5data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L5DataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L5DataApplication.class, args);
+	}
+}
