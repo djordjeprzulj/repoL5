6136ca3ec53e14980aff9c306b5a5bc5d36df1cc
djordje <przulj@gmail.com>
2018-04-05 22:00:16 +0200

    Dodat skript za kreiranje tabele t_store i konfiguracija u application.properties.

---
+++ L5Data/src/main/resources/schema.sql
@@ -0,0 +1,20 @@
+DROP TABLE 't_store';
+
+CREATE TABLE  (
+  id`int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(45) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+insert into t_store values (1, 'Prva prodavnica');
+insert into t_store values (2, 'Druga prodavnica');
+insert into t_store values (3, 'Treća prodavnica');
+insert into t_store values (4, 'Četvrta prodavnica');
+insert into t_store values (5, 'Peta prodavnica');
+insert into t_store values (6, 'Šesta prodavnica');
+insert into t_store values (7, 'Sedma prodavnica');
+insert into t_store values (8, 'Osma prodavnica');
+insert into t_store values (9, 'Deveta prodavnica');
+insert into t_store values (10, 'Deseta prodavnica');
+insert into t_store values (11, 'Jedanaesta prodavnica');
+insert into t_store values (12, 'Dvanaesta prodavnica');
