fde25e83a091a01501742a011c2f0ec53e7f482d
djordje <przulj@gmail.com>
2018-04-06 20:43:06 +0200

    Dodate metode u repozitorijum.

--- L5Data/src/main/java/com/example/l5data/repository/TStoreRepository.java
+++ L5Data/src/main/java/com/example/l5data/repository/TStoreRepository.java
@@ -1,9 +1,16 @@
 package com.example.l5data.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.l5data.model.TStore;
 
 public interface TStoreRepository extends JpaRepository<TStore, Integer> {
-	
+	Iterable<TStore> findByName(String name);
+	Iterable<TStore> findByIdLessThan(int id);  
+	Iterable<TStore> findByNameLike(String likeCriteria);
+	Iterable<TStore> findByNameContaining(String n);
+	Page<TStore> findByNameContainingIgnoreCase(String n, Pageable p);
+
 }
